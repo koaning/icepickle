5795bdc851ce089fc79cbb76849ab4b98d4b5aea
Vincent <vincentwarmerdam@gmail.com>
2022-02-14 22:02:40 +0100

    implement - import this

---
+++ icepickle/this.py
@@ -0,0 +1,18 @@
+print(
+    """
+Roses are red.
+Violets are blue.
+Linear models are simple,
+and they often work too!
+
+Inspecting models is grand,
+understanding them makes you smart.
+Linear systems are not perfect,
+but they're a good place to start.
+
+Advanced Classifier Technology is out there,
+but it can be hard to understand.
+Make sure you read the Illusion of Progress,
+by David J. Hand.
+"""
+)
